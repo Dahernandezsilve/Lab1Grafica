247cfaa407eb7d298bfbb72f744243100a8c7265
Diego Hernández <dahernandezsilvestre@gmail.com>
2023-07-14 23:12:31 -0600

    star

--- main.cpp
+++ main.cpp
@@ -29,13 +29,10 @@ void render() {
 
     setCurrentColor(WHITE);
     std::vector<Vertex2> points = {
-            {413, 177}, {448, 159}, {502, 88}, {553, 53}, {535, 36}, {676, 37}, {660, 52},
-            {750, 145}, {761, 179}, {672, 192}, {659, 214}, {615, 214}, {632, 230}, {580, 230},
-            {597, 215}, {552, 214}, {517, 144}, {466, 180}
+            {165, 380}, {185, 360}, {180, 330}, {207, 345}, {233, 330}, {230, 360}, {250, 380}, {220, 385}, {205, 410}, {193, 383}
     };
-
-    Color fillColor = {255, 0, 0};
-    Color lineColor = {0, 0, 255};
+    Color fillColor = {0, 255, 255};
+    Color lineColor = {255, 255, 255};
     drawPolygon(points,lineColor);
     fillPolygon(points, lineColor, fillColor, clearColor);
 
